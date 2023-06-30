346513a85db0883b1220a26bb540d403284b6a8e
alifnurkhafif <alifkhafif@gmail.com>
2023-06-30 21:53:14 +0700

    first commit

---
+++ src/nativeMain/kotlin/CreateJobExm2.kt
@@ -0,0 +1,11 @@
+import kotlinx.coroutines.*
+
+/*fun main() = runBlocking {
+    val jobJ = launch(start = CoroutineStart.LAZY) {
+        delay(1000L)
+        println("Start new job!")
+    }
+
+    jobJ.join()
+    println("Other task")
+}*/
